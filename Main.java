24ad585fd2eaa4030b3b3a02557f5dce6dcf662b
Oskar Sadkowski <117320665+sadkooski@users.noreply.github.com>
2024-05-04 19:49:54 +0200

    zad4

--- Main.java
+++ Main.java
@@ -1,12 +1,26 @@
+import company.beans.Person;
+import company.interfaces.Messenger;
 import company.implementations.EmailMessenger;
 import company.utils.MathUtils;
 
 public class Main {
     public static void main(String[] args) {
-        int result = MathUtils.add(10, 20);
-        System.out.println("Sum of numbers: " + result);
-        
-        EmailMessenger emailMessenger = new EmailMessenger();
-        emailMessenger.sendMessage("The sum of 10 and 20 is: " + result);
+        Person[] people = new Person[5];
+        final int b = 10;
+
+        try {
+            people[0] = new Person("Carla", 30);
+            people[1] = new Person("Alice", 25);
+            people[2] = new Person("Bob", 35);
+            people[3] = new Person("Carol", 40);
+            people[4] = new Person("David", 45);
+
+            for (Person person : people) {
+                int result = MathUtils.add(person.getAge(), b);
+                person.sendMessage("Wynik dodawania wieku i stałej b dla osoby " + person.getName() + ": " + result);
+            }
+        } catch (Exception e) {
+            System.out.println("Wystąpił błąd: " + e.getMessage());
+        }
     }
 }
