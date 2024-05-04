ef3f04149ed87afb126b1db32d3afddd9948578a
Oskar Sadkowski <117320665+sadkooski@users.noreply.github.com>
2024-05-04 19:25:45 +0200

    zad2

--- Main.java
+++ Main.java
@@ -1,12 +1,8 @@
-import company.beans.Person;
-import company.exceptions.InvalidAgeException;
+import company.implementations.EmailMessenger;
+
 public class Main {
     public static void main(String[] args) {
-        try {
-            Person person = new Person("John Doe", 30);
-            System.out.println("Name: " + person.getName() + ", Age: " + person.getAge());
-        } catch (InvalidAgeException e) {
-            System.err.println("Caught an exception: " + e.getMessage());
-        }
+        EmailMessenger emailMessenger = new EmailMessenger();
+        emailMessenger.sendMessage("Hello, this is a test message.");
     }
 }
